a0e87dbcf84b903a82232bb27777348b955605e2
calvin-gain <calvin@gain.media>
2023-09-18 13:59:37 +0600

    configure mongodb connection

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require("dotenv").config(); 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+
+const { initDbConnection } = require('./mongodb.js')
+
+initDbConnection()
 
 app.get('/', (req, res) => {
     res.send('Hello, it`s aws test')
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Aws test app listening on port ${port}`)
 })
